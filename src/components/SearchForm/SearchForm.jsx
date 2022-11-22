c34f96cd20b3b727f71fd2b833267fbf20bc7737
Valentin <V.Kovalenko.prog@gmail.com>
2022-11-22 21:15:55 +0200

    add search input

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import { useGlobalContext } from '../../context'
-
+import React from "react";
+import { useGlobalContext } from "../../context";
+import './SearchForm.css'
 
 const SearchForm = () => {
-  const { setSearchTerm } = useGlobalContext()
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
 
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
 
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>SearchForm</div>
-  )
-}
+    <section className="sectionSearch">
+      <form className="searchForm" onSubmit={handleSubmit}>
+        <div className="formControl">
+          <label htmlFor="name"> Search Your Favorite Cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+  </section>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
+
+// 198
